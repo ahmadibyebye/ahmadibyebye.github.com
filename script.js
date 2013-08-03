892057ca21137e3c5dc4a94b1e7e98f99edea474
ahmadibyebye <ahmadibyebye>
2013-08-03 09:03:34 +0430

    finishing

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ $(function(){
             ts = new Date(2013, 7, 3,9,0),
             newYear = true;
 
-    if((new Date()) > ts){
-        // The new year is here! Count towards something else.
-        // Notice the *1000 at the end - time must be in milliseconds
-        ts = (new Date()).getTime() + 10*24*60*60*1000;
-        newYear = false;
-    }
-
     $('#countdown').countdown({
         timestamp	: ts,
         callback	: function(days, hours, minutes, seconds){
